e034a7684cd5246dc87e55c731f318985c1a932a
Milix-M <tujikou12dh@gmail.com>
2024-01-22 17:48:07 +0900

    change code

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,14 +1,39 @@
 import '@/styles/globals.css'
+import { Noto_Sans_JP } from 'next/font/google'
 import { NextUIProvider } from '@nextui-org/react'
+import { NextPage } from 'next'
 import type { AppProps } from 'next/app'
 import { useRouter } from 'next/router'
+import { ThemeProvider as NextThemesProvider } from 'next-themes'
 
-export default function App ({ Component, pageProps }: AppProps) {
-  const router = useRouter()
+const notoSansJapanese = Noto_Sans_JP({
+  subsets: ['latin']
+})
+
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: React.ReactElement) => React.ReactNode
+}
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+const App = ({ Component, pageProps }: AppPropsWithLayout) => {
+  const getLayout = Component.getLayout ?? (page => page)
 
   return (
-    <NextUIProvider navigate={router.push}>
-      <Component {...pageProps} />;
+    <NextUIProvider>
+      <NextThemesProvider
+        attribute='class'
+        defaultTheme='dark'
+        enableSystem={false}
+      >
+        <div className={`${notoSansJapanese.className} min-h-screen`}>
+          {getLayout(<Component {...pageProps} />)}
+        </div>
+      </NextThemesProvider>
     </NextUIProvider>
   )
 }
+
+export default App

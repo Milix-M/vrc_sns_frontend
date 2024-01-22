5503f21c1d0185262086797913d831b6da0541a4
Milix-M <tujikou12dh@gmail.com>
2024-01-22 17:31:42 +0900

    add useRouter

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,8 +1,11 @@
 import '@/styles/globals.css'
 import { NextUIProvider } from '@nextui-org/react'
 import type { AppProps } from 'next/app'
+import { useRouter } from 'next/router'
 
 export default function App ({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
   return (
     <NextUIProvider>
       <Component {...pageProps} />;

181afe0eed194769f961fd4e2477b887a606b083
Milix-M <tujikou12dh@gmail.com>
2024-01-28 23:50:12 +0900

    add temp home

---
+++ web/src/pages/home/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import getLayout from "@/components/layouts/non_header";
+
+const Home = () => (
+  <div>
+    <h1>あいうえお　仮置きだよ</h1>
+    <h1>工事中</h1>
+  </div>
+);
+
+Home.getLayout = getLayout;
+
+export default Home;

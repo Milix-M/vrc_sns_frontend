c33d624d49cb4f46911c53e6e4a9c75b4ae4a340
Milix-M <tujikou12dh@gmail.com>
2024-01-30 20:22:19 +0900

    add PostDisplay(Design Only)

---
+++ web/src/components/layouts/main/Post/PostDisplay.tsx
@@ -0,0 +1,56 @@
+import { Avatar, Button } from '@nextui-org/react'
+import { BsThreeDots } from 'react-icons/bs'
+import { FaRegComment, FaRegStar } from 'react-icons/fa'
+import { FaRetweet } from 'react-icons/fa6'
+import { IoBookmarkOutline } from 'react-icons/io5'
+
+const PostDisplay: React.FC = () => {
+  return (
+    <div className='w-full max-w-2xl bg-overlay p-4 rounded-md mt-4'>
+      <div className='flex flex-row'>
+        <div className='post-avator pr-3'>
+          <a href='#'>
+            <Avatar showFallback src='' />
+          </a>
+        </div>
+        <div className=''>
+          <div className='post-header-name flex flex-wrap items-center'>
+            <h3 className='font-bold pr-2'>ユーザー名をここに表示</h3>
+            <span className='font-thin text-sm'>@testuser</span>
+            <time
+              title='2024/1/30 10:10:10'
+              className='font-thin text-sm ml-auto'
+            >
+              数日前
+            </time>
+          </div>
+          <div className='mb-2 text-foreground'>
+            テストの投稿データです。 今日の勤務もお疲れ様でした。
+            明日も頑張ってください。
+          </div>
+          <div className='flex flex-row justify-between'>
+            <Button isIconOnly variant='light'>
+                <FaRegComment size={"1rem"}/>
+            </Button>
+            <Button isIconOnly variant='light'>
+                <FaRetweet size={"1rem"}/>
+            </Button>
+            <Button isIconOnly variant='light'>
+                <FaRegStar size={"1rem"}/>
+            </Button>
+            <div className=''>
+                <Button isIconOnly variant='light'>
+                    <IoBookmarkOutline  size={"1rem"}/>
+                </Button>
+                <Button isIconOnly variant='light'>
+                    <BsThreeDots  size={"1rem"}/>
+                </Button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default PostDisplay

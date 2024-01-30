fb067a5843a2e808389d59a6b7a1fc7daa5f37ab
Milix-M <tujikou12dh@gmail.com>
2024-01-30 21:48:49 +0900

    fix design

--- web/src/components/layouts/main/Post/PostArea.tsx
+++ web/src/components/layouts/main/Post/PostArea.tsx
@@ -4,10 +4,11 @@ import { RiImageAddLine } from 'react-icons/ri'
 import { PiEyeSlash } from 'react-icons/pi'
 import { HiOutlineEmojiHappy } from 'react-icons/hi'
 import { LuSend } from 'react-icons/lu'
+import postAreaCSS from "./PostArea.module.scss"
 
 const PostArea: React.FC = () => {
   return (
-    <div className='w-full max-w-2xl my-auto bg-overlay p-4 rounded-md'>
+    <div className={`${postAreaCSS.postArea} bg-overlay p-4 rounded-md`}>
       <div className='flex flex-col rounded-md p-1 mb-2 text-foreground placeholder:focus'>
         <Textarea placeholder='いまなにしてはるん？' />
         <div className='mt-3 flex flex-1 flex-row flex-wrap'>

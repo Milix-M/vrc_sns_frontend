36f427717ddf31c1910d8fcf32bf3f4ed1503082
Milix-M <tujikou12dh@gmail.com>
2024-01-30 20:54:59 +0900

    add temp postlist

---
+++ web/src/components/layouts/main/Post/PostList.tsx
@@ -0,0 +1,18 @@
+import PostDisplay from "./PostDisplay"
+
+const PostList: React.FC = () => {
+    return (
+        <div>
+            <PostDisplay />
+            <PostDisplay />
+            <PostDisplay />
+            <PostDisplay />
+            <PostDisplay />
+            <PostDisplay />
+            <PostDisplay />
+            <PostDisplay />
+        </div>
+    )
+}
+
+export default PostList

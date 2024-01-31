a57e45331d870b50f15c89be2f62dcfea39568cf
Milix-M <tujikou12dh@gmail.com>
2024-02-01 01:23:03 +0900

    PWAの設定を追加

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
+import nextPWA from 'next-pwa'
+
+const withPWA = nextPWA({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+})
+
 const nextConfig = {
   reactStrictMode: true,
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);

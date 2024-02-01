a0eebd241937968cc6cb3e577f880eaf7eba8412
Milix-M <tujikou12dh@gmail.com>
2024-02-01 20:03:21 +0900

    run format

--- web/src/components/layouts/main/Post/PostArea.tsx
+++ web/src/components/layouts/main/Post/PostArea.tsx
@@ -4,71 +4,81 @@ import { RiImageAddLine } from 'react-icons/ri'
 import { PiEyeSlash } from 'react-icons/pi'
 import { HiOutlineEmojiHappy } from 'react-icons/hi'
 import { LuSend } from 'react-icons/lu'
-import postAreaCSS from "./PostArea.module.scss"
+import postAreaCSS from './PostArea.module.scss'
 import { useState } from 'react'
 import axios from 'lib/axios'
-import data from "@emoji-mart/data/sets/14/twitter.json";
-import Picker from "@emoji-mart/react";
+import data from '@emoji-mart/data/sets/14/twitter.json'
+import Picker from '@emoji-mart/react'
 
 const PostArea: React.FC = () => {
-  const [content, setContent] = useState<string>("")
+  const [content, setContent] = useState<string>('')
   const [isPickerOpened, setIsPickerOpened] = useState<boolean>(false)
 
   const handlePost = async () => {
     if (!content.trim()) {
-      alert("投稿内容は必須です")
+      alert('投稿内容は必須です')
       return
     }
 
     const requestBody = {
-      content,
+      content
     }
 
     try {
-      await axios.post("/api/post/create", requestBody)
+      await axios.post('/api/post/create', requestBody)
 
-      setContent("")
+      setContent('')
     } catch (err) {
-      console.log("Error posting data:", err)
+      console.log('Error posting data:', err)
     }
   }
 
   return (
-    <div className="w-full bg-overlay p-4 rounded-md">
+    <div className='w-full bg-overlay p-4 rounded-md'>
       <div className='flex flex-col rounded-md p-1 mb-2 text-foreground placeholder:focus'>
         <Textarea
           placeholder='いまなにしてはるん？'
           value={content}
-          onChange={(e) => setContent(e.target.value)}
+          onChange={e => setContent(e.target.value)}
         />
         <div className='mt-3 flex flex-1 flex-row flex-wrap justify-between relative'>
           <div className='flex flex-row mr-2'>
-            <Tooltip content="画像を追加">
+            <Tooltip content='画像を追加'>
               <Button isIconOnly variant='light'>
                 <RiImageAddLine color='#2b94ff' size={'1.4rem'} />
               </Button>
             </Tooltip>
 
-            <Tooltip content="投票を追加">
+            <Tooltip content='投票を追加'>
               <Button isIconOnly variant='light'>
                 <CgPoll color='#2b94ff' size={'1.4rem'} />
               </Button>
             </Tooltip>
 
-            <Tooltip content="CWを設定">
+            <Tooltip content='CWを設定'>
               <Button isIconOnly variant='light'>
                 <PiEyeSlash color='#2b94ff' size={'1.4rem'} />
               </Button>
             </Tooltip>
 
-            <Tooltip content="絵文字ピッカー">
-              <Button isIconOnly variant='light' onClick={() => setIsPickerOpened(!isPickerOpened)}>
+            <Tooltip content='絵文字ピッカー'>
+              <Button
+                isIconOnly
+                variant='light'
+                onClick={() => setIsPickerOpened(!isPickerOpened)}
+              >
                 <HiOutlineEmojiHappy color='#2b94ff' size={'1.4rem'} />
               </Button>
             </Tooltip>
-            { isPickerOpened && (
-              <div className="absolute z-50 top-[40px] w-full">
-                <Picker data={data} onEmojiSelect={console.log} set="twitter" dynamicWidth={true} onClickOutside={() => setIsPickerOpened(false)}/>
+            {isPickerOpened && (
+              <div className='absolute z-50 top-[40px] w-full'>
+                <Picker
+                  data={data}
+                  onEmojiSelect={console.log}
+                  set='twitter'
+                  dynamicWidth={true}
+                  onClickOutside={() => setIsPickerOpened(false)}
+                />
               </div>
             )}
           </div>

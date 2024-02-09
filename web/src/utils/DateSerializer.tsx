536b533b0ac97559e160c8a7a9d4d90f9a2653dc
Milix-M <tujikou12dh@gmail.com>
2024-02-09 21:52:18 +0900

    DateSerializer add dateonly case

--- web/src/utils/DateSerializer.tsx
+++ web/src/utils/DateSerializer.tsx
@@ -1,10 +1,15 @@
 import { format, formatDistanceToNowStrict } from 'date-fns'
 import { ja } from 'date-fns/locale'
 
-const dateSerializer = (date: unknown, formatType: 'standard' | 'distance') => {
+const dateSerializer = (
+  date: unknown,
+  formatType: 'standard' | 'dateonly' | 'distance'
+) => {
   switch (formatType) {
     case 'standard':
       return format(date as Date, 'yyyy-MM-dd HH:mm:ss', { locale: ja })
+    case 'dateonly':
+      return format(date as Date, 'yyyy/MM/dd', { locale: ja })
     case 'distance':
       return formatDistanceToNowStrict(date as Date, { locale: ja })
   }

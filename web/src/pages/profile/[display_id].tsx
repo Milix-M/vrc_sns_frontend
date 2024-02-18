1aaaa1f77f300af4bd836df180a91512cf8d23dd
Milix-M <tujikou12dh@gmail.com>
2024-02-19 01:33:14 +0900

    ボタンで過去のポストも表示できるようにした

--- web/src/pages/profile/[display_id].tsx
+++ web/src/pages/profile/[display_id].tsx
@@ -1,15 +1,30 @@
 import { useRouter } from 'next/router'
-import React, { useState } from 'react'
+import React from 'react'
 import getLayout from '@/components/layouts/main'
 import ProfileDetail from '@/components/layouts/main/Profile/ProfileDetail'
 import PostDisplay from '@/components/layouts/main/Post/PostDisplay'
-import { useUserInfo, useUserPosts } from '@/hooks/GetUserDatas'
+import { useUserInfo } from '@/hooks/GetUserDatas'
+import { PostType } from 'lib/types'
+import useSWRInfinite from 'swr/infinite'
+import fetcher from 'lib/fetcher'
+import { Button } from '@nextui-org/react'
 
 const UserProfile = () => {
   const router = useRouter()
   const display_id = router.query.display_id as string
   const userData = useUserInfo(display_id)
-  const userPosts = useUserPosts(display_id, 10)
+
+  const getKey = (pageIndex: number, previousPageData: PostType[][]) => {
+    if (pageIndex === 0) return `/api/users/${display_id}/posts?limit=10`
+    if (previousPageData.flat().at(-1)?.postid === undefined) return null
+    return `/api/users/${display_id}/posts?limit=10&untilid=${previousPageData.flat().at(-1)?.postid}`
+  }
+
+  const { data, size, setSize } = useSWRInfinite(getKey, fetcher, {
+    revalidateIfStale: false, // キャッシュがあっても再検証
+    revalidateOnFocus: false, // windowをフォーカスすると再検証
+    revalidateFirstPage: false, // 2ページ目以降を読み込むとき毎回1ページ目を再検証
+  })
 
 
   return (
@@ -17,9 +32,15 @@ const UserProfile = () => {
       { router.isReady && (
         <>
           <ProfileDetail userData={userData.userData} />
-          {userPosts.userPosts?.map((post) => (
-            <PostDisplay userData={userData.userData} postData={post} key={post.postid}/>
+          {data?.flat().map((post, index) => (
+            <PostDisplay userData={post.userData} postData={post} key={post.postid}/>
           ))}
+
+          <Button
+            onClick={() => {
+              setSize(size + 1)
+            }}
+          ></Button>
         </>
       )}
     </div>
